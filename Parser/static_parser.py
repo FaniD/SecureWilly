03e1eef06c69a51acf7e6269568e50c2da1b4309
Ubuntu <ubuntu@docker-security.novalocal>
2017-11-25 18:30:22 +0000

    Fixed new dockerfile rules and added comments to profile

--- Parser/static_parser.py
+++ Parser/static_parser.py
@@ -1,62 +1,120 @@
 #!/usr/bin/env python
 import io
 import sys
+from collections import OrderedDict
+
+#Function used to delete duplicates from a list - profile rules in our case
+def ordered_set(in_list):
+    out_list = []
+    added = set()
+    for val in in_list:
+        if not val in added:
+            out_list.append(val)
+            added.add(val)
+    return out_list
 
 static_profile = []
-static_profile.append('#include <tunables/global>\n\n')
-static_profile.append('profile static_profile flags=(attach_disconnected,mediate_deleted) {\n')
 
 #Dockerfile
 dockerfile = str(sys.argv[1])
 
 with open(dockerfile,'r') as infile:
 	data = infile.readlines()
-	static_profile.append('\tfile,\n') #this rule is needed so that I can work with files (create files, dirs, copy, etc)
+
+#Rules
+
+#This rule is needed so that we can work with files (create files/directories, copy, etc)
+file_rule = '\t#This rule is needed so that I can work with files (create files/directories, copy, etc)\n\tfile,\n\n'
+
+#These rules are needed so that we can switch between users
+setuid_setgid_rule = '\t#Chown or User command\n\t#These rules are needed so that we can switch between users\n\tcapability setuid,\n\tcapability setgid,\n\n'
+
+#Chown capability
+chown_cap = '\t#This capability is needed to use chown\n\tcapability chown,\n\n'
+
+#static_profile.append(file_rule)
 
 #Search for chmod or chown in Dockerfile
 chmod = 'RUN chmod'
 chown = 'RUN chown'
+user = 'USER'
 
 for line in data:
-	if chmod in line: #chmod found
+
+        if user in line:
+        #USER command is found so we consider that there must be given the permission to switch between users
+        #There should be permission to switch only to this user but athough it is given in the documentation, this specification is not yet implemented:
+        #setuid -> userA
+                static_profile.append(setuid_setgid_rule)
+
+	if chmod in line:
+        #Chmod found so we have to deal with files (file rule) and fix sticky bits and permissions
 		line = line.strip('\n')
 		line = line.split(' ')
 		#flags	TODO	s = line[1].split('-', 1)
-		#Chmod Rule - not supported 
-		#instead of this rule we add w permission to file so that it can be "chmod"ed...
-		#chmod_rule = '\tchmod ' + line[len(line)-2] + ' ' + line[len(line)-1] + ',\n'
-		chmod_rule = '\t' + line[len(line)-1] + ' w,\n'
+
+                #Chmod Rule - not supported 
+		#Add the right permissions to owner of the file and others
 
 		#Path permission rule - File access rule
+                chmod_path = line[len(line)-1]
 		chmod_permission = list(line[len(line)-2])
-		chmod_path = line[len(line)-1]
-		#chmod permissions calculate both for letters and numbers. ONLY FOR OWNER!
-		if chmod_permission[1] == 'u':
-			permissions = line[len(line)-2].split('+')
-		if chmod_permission[1] == '1':
-			permissions = 'ix'
-		if chmod_permission[1] == '2':
-			permissions = 'w'
-		if chmod_permission[1] == '3':
-			permissions = 'wix'
-		if chmod_permission[1] == '4':
-			permissions = 'r'
-		if chmod_permission[1] == '5':
-			permissions = 'rix'
-		if chmod_permission[1] == '6':
-			permissions = 'rw'
-		if chmod_permission[1] == '7':
-			permissions = 'rwix'
-		#Supported only for owner's permissions
-		chmod_path_rule = '\towner ' + chmod_path + ' ' + permissions + ',\n'
 
-		#Add rules to AppArmor profile
-		static_profile.append(chmod_rule)
-		static_profile.append(chmod_path_rule)
 
-	if chown in line: #chown found
-		#Add capability rule
-		static_profile.append('\tcapability chown,\n')
+                #chmod permissions calculate both for letters and numbers. ONLY FOR OWNER and OTHERS. Not supported for owning group!
+		if chmod_permission[0] == 'u':
+			owner = line[len(line)-2].split('+')
+		if chmod_permission[0] == '1':
+			owner = 'ix'
+		if chmod_permission[0] == '2':
+			owner = 'w'
+		if chmod_permission[0] == '3':
+			owner = 'wix'
+		if chmod_permission[0] == '4':
+			owner = 'r'
+		if chmod_permission[0] == '5':
+			owner = 'rix'
+		if chmod_permission[0] == '6':
+			owner = 'rw'
+		if chmod_permission[0] == '7':
+			owner = 'rwix'
+
+                if chmod_permission[2] == 'u':
+                        others = line[len(line)-2].split('+')
+                if chmod_permission[2] == '1':
+                        others = 'ix'
+                if chmod_permission[2] == '2':
+                        others = 'w'
+                if chmod_permission[2] == '3':
+                        others = 'wix'
+                if chmod_permission[2] == '4':
+                        others = 'r'
+                if chmod_permission[2] == '5':
+                        others = 'rix'
+                if chmod_permission[2] == '6':
+                        others = 'rw'
+                if chmod_permission[2] == '7':
+                        others = 'rwix'
+
+                #Owner's permissions
+		chmod_owner = '\towner ' + chmod_path + ' ' + owner + ',\n'
+                #Others' permissions
+                chmod_others = '\t' + chmod_path + ' ' + others  + ',\n'
+
+                chmod_rule = '\t#Chmod command\n' + chmod_owner + chmod_others + '\n'
+                static_profile.append(chmod_rule)
+                static_profile.append(file_rule)
+
+	if chown in line:
+        #Chown command found so we need file rule, setuid rule and sticky bits - if given
+
+		#Add capability rule if we want to allow chown command to be used in the container
+#?????not sure		static_profile.append('\tcapability chown,\n')
+
+                chown_line = '\t#Chown command\n' + chown_cap
+                static_profile.append(chown_line)
+                static_profile.append(file_rule)
+                static_profile.append(setuid_setgid_rule)
 
 		#Not supported!
 		#Chown Rule needed as well
@@ -83,7 +141,7 @@ for line in data:
 #		static_profile.append(src)
 #		static_profile.append(dst)
 
-static_profile.append('\n')
+#static_profile.append('\n')
 
 #DockerCompose - if it exists
 if (len(sys.argv) > 2):
@@ -168,7 +226,23 @@ if (len(sys.argv) > 2):
 		#if rlimit in line:
 			
 			
+#static_profile.append('}\n')
+
+#Delete duplicate rules by converting list to set. Convert back to list to keep the order of the beggining and ending of a profile
+#This is the way to delete duplicates, when we don't care about the order
+static_profile = list(set(static_profile))
+
+#Delete duplicates, but keeping the order JUST FOR THE PRESENTATION in order to show the extracted rules from the correct orders
+#static_profile = ordered_set(static_profile)
+
+#Add the beggining and ending of the profile - do it in both inordered and not inoerdered lists
+#beggining
+static_profile.insert(0, '#include <tunables/global>\n\nprofile static_profile flags=(attach_disconnected,mediate_deleted) {\n\n')
+#ending
 static_profile.append('}\n')
+
+
+#Output
 with open('static_profile', 'w') as outfile:
 	outfile.writelines( static_profile )
 
