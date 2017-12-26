c2e0bb28fd01ecde3a220720c5d6ae80c5f0f440
Ubuntu <ubuntu@docker-security3.novalocal>
2017-12-26 22:31:37 +0000

    Mount example fixing

--- Docker_tests/mount/create.sh
+++ Docker_tests/mount/create.sh
@@ -2,5 +2,6 @@
 echo "ls mount_here (it should be empty)"
 ls ./mount_here
 echo "Creating a new file in /mount_here"
+ls -l / | grep mount_here
 echo "greetings" > /mount_here/hello
 echo "Now check volume data to see if file hello exists"
