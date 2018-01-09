68357011b48263282e7afe2a5c38d10343b6a5b3
Ubuntu <ubuntu@docker-security7.novalocal>
2018-01-09 22:57:22 +0000

    A new script and changes at profiles.

--- media-streaming/clear_containers.sh
+++ media-streaming/clear_containers.sh
@@ -5,5 +5,3 @@ docker container rm streaming_dataset
 docker container rm streaming_server
 docker container rm streaming_client
 
-echo > /dev/null | sudo tee /var/log/syslog
-echo > /dev/null | sudo tee /var/log/kern.log
---
+++ media-streaming/clear_logs.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo > /dev/null | sudo tee /var/log/syslog
+echo > /dev/null | sudo tee /var/log/kern.log
