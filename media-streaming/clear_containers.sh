8d97af8f776662c23c8437be66c71df260968acb
Ubuntu <ubuntu@docker-security7.novalocal>
2018-01-09 22:31:53 +0000

    Added a requirements script to use to new vms

--- media-streaming/clear_containers.sh
+++ media-streaming/clear_containers.sh
@@ -4,3 +4,6 @@ docker container kill streaming_server
 docker container rm streaming_dataset
 docker container rm streaming_server
 docker container rm streaming_client
+
+echo > /dev/null | sudo tee /var/log/syslog
+echo > /dev/null | sudo tee /var/log/kern.log
---
+++ requirements.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo apt-get update
+
+#Upgrade docker-engine
+sudo apt-get upgrade docker-engine
+
+#Install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
