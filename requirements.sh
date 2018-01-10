12449256029876550880cc958e67b7643e8d7d21
Ubuntu <ubuntu@docker-security8.novalocal>
2018-01-10 01:12:52 +0000

    Apparmor utils needed in my VMs

--- requirements.sh
+++ requirements.sh
@@ -7,3 +7,6 @@ sudo apt-get upgrade docker-engine
 #Install docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+#Apparmor utils
+sudo apt-get install apparmor-utils
