6a915ed8a641cb155ff0c30d7d9c1dcbf2b77884
Ubuntu <ubuntu@docker-security7.novalocal>
2018-01-14 03:36:08 +0000

    Awk script produces network kern logs for create

--- Parser/awk_it.sh
+++ Parser/awk_it.sh
@@ -2,11 +2,22 @@
 
 for SERVICE in server client dataset; do
 #for N in 1 ... ${SERVICES} ; do
-	awk '/capability/ {print $16;}' ../media-streaming/audit_messages/complain_messages/kernlogs_${SERVICE} > caps/caps1
-	awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' caps/caps1 > caps/caps1_${SERVICE}
-	rm caps/caps1
 
-	awk '/capability/ {print $11;}' ../media-streaming/audit_messages/complain_messages/dmesg_${SERVICE} > caps/caps2
-	awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' caps/caps2 > caps/caps2_${SERVICE}
-	rm caps/caps2
+	#Capabilities
+	#kern logs
+	awk '/capability/ {print $16;}' ../media-streaming/audit_messages/complain_messages/kernlogs_${SERVICE} > tmp_file
+	awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' tmp_file > awk_out/caps1_${SERVICE}
+	rm tmp_file
+
+	#dmesg logs
+	awk '/capability/ {print $11;}' ../media-streaming/audit_messages/complain_messages/dmesg_${SERVICE} > tmp_file
+	awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' tmp_file > awk_out/caps2_${SERVICE}
+	rm tmp_file
+
+	#Network
+	#kern logs
+	awk '/create/ {print $15 ',' $16;}' ../media-streaming/audit_messages/complain_messages/kernlogs_${SERVICE} > tmp_file
+	awk 'BEGIN {FS="=| ";} {gsub(/"/,"",$2); gsub(/"/,"",$4); print $2 ',' $4;}' tmp_file > awk_out/net1_${SERVICE}
+	rm tmp_file
+
 done
