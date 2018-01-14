a7fff9a0a036447ad5acb641f91e70e2116f614a
Ubuntu <ubuntu@docker-security7.novalocal>
2018-01-14 03:13:50 +0000

    Script produces caps of all services

--- Parser/awk_it.sh
+++ Parser/awk_it.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-LOGS="dmesg kernlogs"
-#SERVICE="server client dataset"
+for SERVICE in server client dataset; do
+#for N in 1 ... ${SERVICES} ; do
+	awk '/capability/ {print $16;}' ../media-streaming/audit_messages/complain_messages/kernlogs_${SERVICE} > caps/caps1
+	awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' caps/caps1 > caps/caps1_${SERVICE}
+	rm caps/caps1
 
-for N in 1 ... ${SERVICES} ; do
-	awk '/capability/ {print $16;}' kernlogs_${SERVICE} > caps
-	awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' caps
-
-	awk '/capability/ {print $11;}' dmesg_${SERVICE} > caps
-	awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' caps
+	awk '/capability/ {print $11;}' ../media-streaming/audit_messages/complain_messages/dmesg_${SERVICE} > caps/caps2
+	awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' caps/caps2 > caps/caps2_${SERVICE}
+	rm caps/caps2
+done
