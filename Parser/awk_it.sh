5271cd206085613f9a481315f7b73430cfc225fc
Ubuntu <ubuntu@docker-security7.novalocal>
2018-01-14 15:29:18 +0000

    Dmesg create network fixed in awk_it

--- Parser/awk_it.sh
+++ Parser/awk_it.sh
@@ -5,19 +5,35 @@ for SERVICE in server client dataset; do
 
 	#Capabilities
 	#kern logs
+	#Find lines that include keyword "capability"
 	awk '/capability/ {print $16;}' ../media-streaming/audit_messages/complain_messages/kernlogs_${SERVICE} > tmp_file
-	awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' tmp_file > awk_out/caps1_${SERVICE}
-	rm tmp_file
+	#Strip lines with capability to keep just the capname of each
+	awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' tmp_file > awk_out/caps_${SERVICE}
+	echo '' > tmp_file
 
 	#dmesg logs
+	#Find lines that include keyword "capability"
 	awk '/capability/ {print $11;}' ../media-streaming/audit_messages/complain_messages/dmesg_${SERVICE} > tmp_file
-	awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' tmp_file > awk_out/caps2_${SERVICE}
-	rm tmp_file
+	#Strip lines with capability to keep just the capname of each
+	awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' tmp_file >> awk_out/caps_${SERVICE}
+	echo '' > tmp_file
 
 	#Network
 	#kern logs
+	#Find lines that include keyword "create" for network - keep family and sock_type
+	#Omit protocol, apparmor network rule needs at least 2 parameters
 	awk '/create/ {print $15 ',' $16;}' ../media-streaming/audit_messages/complain_messages/kernlogs_${SERVICE} > tmp_file
-	awk 'BEGIN {FS="=| ";} {gsub(/"/,"",$2); gsub(/"/,"",$4); print $2 ',' $4;}' tmp_file > awk_out/net1_${SERVICE}
-	rm tmp_file
+	#Strip lines with family and sock_type to keep just the tag of each
+	awk 'BEGIN {FS="=| ";} {gsub(/"/,"",$2); gsub(/"/,"",$4); print $2 ',' $4;}' tmp_file > awk_out/net_${SERVICE}
+	echo '' > tmp_file
 
+	#dmesg logs
+	#Find lines that include keyword "create" for network - keep family and sock_type
+	#Omit protocol, apparmor network rule needs at least 2 parameters
+	awk '/create/ {print $10 ',' $11;}' ../media-streaming/audit_messages/complain_messages/dmesg_${SERVICE} > tmp_file
+	#Strip lines with family and sock_type to keep just the tag of each
+	awk 'BEGIN {FS="=| ";} {gsub(/"/,"",$2); gsub(/"/,"",$4); print $2 ',' $4;}' tmp_file >> awk_out/net_${SERVICE}
+	echo '' > tmp_file
+
+	rm tmp_file
 done
--- media-streaming/audit_messages/complain_messages/awk_it.sh
+++
@@ -1,4 +0,0 @@
-awk '/capability/ {print $16;}' kernlogs_server
-#awk 'BEGIN {FS="=";} {print $2;}' caps
-awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' caps
-#then strip "" with python
