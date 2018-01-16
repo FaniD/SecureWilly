27ffcdd4e640db008716eee8b500316c0888fe60
Ubuntu <ubuntu@docker-security8.novalocal>
2018-01-16 17:12:24 +0000

    Network, capabilities, file access rules ready for awk

--- Parser/awk_it.sh
+++ Parser/awk_it.sh
@@ -3,59 +3,67 @@
 for SERVICE in server client dataset; do
 #for N in 1 ... ${SERVICES} ; do
 
-	#Capabilities
+	#~~~Capabilities~~~
+
 	#kern logs
 	#Find lines that include keyword "capability"
-	awk '/capability/ {for(i=1;i<=NF;i++) {if($i ~ /capname/) print $i}}' ../media-streaming/audit_messages/complain_messages/kernlogs_${SERVICE} > tmp_file
+	awk '/capability/ {for(i=1;i<=NF;i++) {if($i ~ /capname/) print $i}}' ../media-streaming/audit_messages/complain_messages/new/kernlogs_${SERVICE} > tmp_file
 	#Strip lines with capability to keep just the capname of each
 	awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' tmp_file > awk_out/caps_${SERVICE}
 
 	#dmesg logs
 	#Find lines that include keyword "capability"
-	awk '/capability/ {for(i=1;i<=NF;i++) {if($i ~ /capname/) print $i}}' ../media-streaming/audit_messages/complain_messages/dmesg_${SERVICE} > tmp_file
+	awk '/capability/ {for(i=1;i<=NF;i++) {if($i ~ /capname/) print $i}}' ../media-streaming/audit_messages/complain_messages/new/dmesg_${SERVICE} > tmp_file
 	#Strip lines with capability to keep just the capname of each
 	awk 'BEGIN {FS="=";} {gsub(/"/,"",$2); print $2;}' tmp_file >> awk_out/caps_${SERVICE}
 
 	#Remove duplicates
-	awk '!seen[$0]++' awk_out/caps_${SERVICE} > awk_out/rcaps_${SERVICE}
+	awk '!seen[$0]++' awk_out/caps_${SERVICE} > awk_out/complainlogs_caps_${SERVICE}
 
-	#Network
+
+	#~~~Network~~~
+	
 	#All net permissions
 	for NET in create accept bind connect listen read write send receive getsockname getpeername getsockopt setsockopt fcntl ioctl shutdown getpeersec; do
 		#kern logs
 		#Find lines that include keyword "create" for network - keep family and sock_type
 		#Omit protocol, apparmor network rule needs at least 2 parameters
-		awk -v net="$NET" '/net/ {for(i=1;i<=NF;i++) {{if($i ~ /family/) printf "%s", $i} {if($i ~ /sock_type/) print "", $i}}}' ../media-streaming/audit_messages/complain_messages/kernlogs_${SERVICE} > tmp_file
-
+		awk -v net="$NET" '/net/ {for(i=1;i<=NF;i++) {{if($i ~ /family/) printf "%s", $i} {if($i ~ /sock_type/) print "", $i}}}' ../media-streaming/audit_messages/complain_messages/new/kernlogs_${SERVICE} > tmp_file
 		#Strip lines with family and sock_type to keep just the tag of each
 		awk 'BEGIN {FS="=| ";} {gsub(/"/,"",$2); gsub(/"/,"",$4); print $2 ',' $4;}' tmp_file >> awk_out/net_${SERVICE}
 
 		#dmesg logs
 		#Find lines that include keyword "create" for network - keep family and sock_type
 		#Omit protocol, apparmor network rule needs at least 2 parameters
-		awk -v net="$NET" '/net/ {for(i=1;i<=NF;i++) {{if($i ~ /family/) printf "%s", $i} {if($i ~ /sock_type/) print "", $i}}}' ../media-streaming/audit_messages/complain_messages/dmesg_${SERVICE} > tmp_file
+		awk -v net="$NET" '/net/ {for(i=1;i<=NF;i++) {{if($i ~ /family/) printf "%s", $i} {if($i ~ /sock_type/) print "", $i}}}' ../media-streaming/audit_messages/complain_messages/new/dmesg_${SERVICE} > tmp_file
 		#Strip lines with family and sock_type to keep just the tag of each
 		awk 'BEGIN {FS="=| ";} {gsub(/"/,"",$2); gsub(/"/,"",$4); print $2 ',' $4;}' tmp_file >> awk_out/net_${SERVICE}
 	done
 
         #Remove duplicates
-	awk '!seen[$0]++' awk_out/net_${SERVICE} > awk_out/rnet_${SERVICE}
+	awk '!seen[$0]++' awk_out/net_${SERVICE} > awk_out/complainlogs_net_${SERVICE}
 
-	#File access rules
+
+	#~~~File access rules~~~
+
 	for OPERATION in create open delete rename read getattr getxattr write append trunc setattr setxattr chmod chown chgrp link snapshot lock mmap mprot exec change_profile onexec exectime; do
 		#mmap_r mmap_w mmap_x mprot_wx mprot_xw 
-		awk -v operation="$OPERATION" '/operation/ {for(i=1;i<=NF;i++) {{if($i ~ /name/) printf "%s", $i} {if($i ~ /requested_mask/) print "", $i}}}' ../media-streaming/audit_messages/complain_messages/new/kernlogs_${SERVICE} > tmp_file
 
+		#kernlogs
+		#Find lines that include keyword "operation" from the operations loop - keep name and requested_mask
+		awk -v operation="$OPERATION" '/operation/ {for(i=1;i<=NF;i++) {{if($i ~ /name/) printf "%s", $i} {if($i ~ /requested_mask/) print "", $i}}}' ../media-streaming/audit_messages/complain_messages/new/kernlogs_${SERVICE} > tmp_file
+		#Strip lines with name and requested_mask to keep just the tag of each
 		awk 'BEGIN {FS="=| ";} {gsub(/"/,"",$2); gsub(/"/,"",$4); print $2 ',' $4;}' tmp_file >> awk_out/file_${SERVICE}
 
-#ADD DMESG HERE...
+		#dmesg logs
+		#Find lines that include keyword "operation" from the operations loop - keep name and requested_mask
+		awk -v operation="$OPERATION" '/operation/ {for(i=1;i<=NF;i++) {{if($i ~ /name/) printf "%s", $i} {if($i ~ /requested_mask/) print "", $i}}}' ../media-streaming/audit_messages/complain_messages/new/dmesg_${SERVICE} > tmp_file
+		#Strip lines with name and requested_mask to keep just the tag of each
+		awk 'BEGIN {FS="=| ";} {gsub(/"/,"",$2); gsub(/"/,"",$4); print $2 ',' $4;}' tmp_file >> awk_out/file_${SERVICE}
 	done
 
 	#Remove duplicates
-	awk '!seen[$0]++' awk_out/file_${SERVICE} > awk_out/rfile_${SERVICE}
+	awk '!seen[$0]++' awk_out/file_${SERVICE} > awk_out/complainlogs_file_${SERVICE}
 done
-
-#awk '!seen[$0]++' ----> OMIT DUPLICATES
-
 
 rm tmp_file
