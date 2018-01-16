e3f7d8c26fb7e45ebcb324bb0b76dbba7a1509f3
Ubuntu <ubuntu@docker-security8.novalocal>
2018-01-16 21:46:52 +0000

    Pre enforce mode test

--- media-streaming/complain_mode_3a.sh
+++ media-streaming/complain_mode_3a.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo aa_complain /etc/apparmor.d/dataset_profile
-sudo aa_complain /etc/apparmor.d/server_profile
-sudo aa_complain /etc/apparmor.d/client_profile
+sudo aa-complain /etc/apparmor.d/dataset_profile
+sudo aa-complain /etc/apparmor.d/server_profile
+sudo aa-complain /etc/apparmor.d/client_profile
--- media-streaming/enforce_mode_3b.sh
+++ media-streaming/enforce_mode_3b.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #If profiles are in complain mode, turn them in enforce mode
-sudo aa_enforce /etc/apparmor.d/dataset_profile
-sudo aa_enforce /etc/apparmor.d/server_profile
-sudo aa_enforce /etc/apparmor.d/client_profile
+sudo aa-enforce /etc/apparmor.d/dataset_profile
+sudo aa-enforce /etc/apparmor.d/server_profile
+sudo aa-enforce /etc/apparmor.d/client_profile
