7e65aa8aeaefaccb5953f4a98a6d027ba41e62c2
Ubuntu <ubuntu@docker-security8.novalocal>
2018-04-29 13:21:18 +0000

    Non docker script for genprof testing

--- test_genprof/capabilities/genprof/chown_it.sh
+++ test_genprof/capabilities/genprof/chown_it.sh
@@ -5,6 +5,6 @@ echo "greetings" > hello
 echo "ls -l to see /hello permissions"
 ls -l / | grep hello
 echo "Chown for UserA and file /hello"
-chown userA:userA /hello
+chown userA:userA ./hello
 echo "Now ls -l to see if it worked"
 ls -l / | grep hello
