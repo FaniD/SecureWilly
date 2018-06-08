2176e5db3486e1ff01963722ffee6de8b293afdf
Ubuntu <ubuntu@docker-security8.novalocal>
2018-06-08 21:12:23 +0000

    Fixing some last details to the comparison of genprof and logs_based profile

--- genprof_compare/docker_image/fix_prof.sh
+++ genprof_compare/docker_image/fix_prof.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo cp profile_created_by_genprof /etc/apparmor.d/
-sudo apparmor_parser -r -W /etc/apparmor.d/profile_created_by_genprof
-sudo aa-enforce /etc/apparmor.d/profile_created_by_genprof
+sudo cp prof_by_no_audit_logs /etc/apparmor.d/
+sudo apparmor_parser -r -W /etc/apparmor.d/prof_by_no_audit_logs
+sudo aa-enforce /etc/apparmor.d/prof_by_no_audit_logs
