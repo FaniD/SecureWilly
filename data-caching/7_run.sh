eef720a32fb36cca4375c87555e8af429a3d9f04
Ubuntu <ubuntu@docker-security8.novalocal>
2018-06-12 23:45:36 +0000

    Fixing scripts for data caching benchmark

--- data-caching/7_run.sh
+++ data-caching/7_run.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 docker run --name dc-server --net caching_network -d cloudsuite/data-caching:server -t 4 -m 4096 -n 550
+docker run -it --name dc-client --net caching_network cloudsuite/data-caching:client bash
+cd /usr/src/memcached/memcached_client/
