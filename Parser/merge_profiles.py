af4b33b3dd6cddf15d2e5003cd8e805233ee7d7f
Ubuntu <ubuntu@docker-security7.novalocal>
2018-06-16 20:05:19 +0000

    Fixing merge_profiles.py

--- Parser/merge_profiles.py
+++ Parser/merge_profiles.py
@@ -8,15 +8,17 @@ new_profile = []
 
 print 'Give name of service, version and mode\n'
 service = str(sys.argv[1])
-version = str(sys.argv[2]) #New version
-mode = str(sys.argv[3])
+version = str(sys.argv[2]) #New version! number here
+mode = str(sys.argv[3]) #complain, enforce, complain_audit, enforce_audit
 
 round_ = int(version)-1
 old_profile = 'profiles/' + service + '/version_' + str(round_)
-awk_logs = 'Logs/RUN' + version +'/awk_out/
+awk_caps = 'Logs/RUN' + version +'/awk_out/' + mode + '_logs_caps_' + service
+awk_net = 'Logs/RUN' + version +'/awk_out/' + mode + '_logs_net_' + service
+awk_file = 'Logs/RUN' + version +'/awk_out/' + mode + '_logs_file_' + service
 
 with open(old_profile,'r') as infile:
-	data = infile.readlines()
+    data = infile.readlines()
 
 #This rule is needed so that we can work with files (create files/directories, copy, etc)
 file_rule = '\tfile,  #This rule is needed so that I can work with files (create files/directories, copy, etc)\n'
@@ -32,48 +34,65 @@ base = []
 
 for line in data:
 
-        if include in line:
-            if not tunables in line:
-                base.append(line)
-        if line.startswith(profile):
-                base.append('#include <tunables/global>\n\n')
-                base.append(line)
-                base.append(file_rule)
+    if include in line:
+        if not tunables in line:
+        base.append(line)
+    if line.startswith(profile):
+        base.append('#include <tunables/global>\n\n')
+        base.append(line)
+        base.append(file_rule)
 
         #Base is ready
         #Add all the rules from here on
         #Except for the closure '}'
 
-        if not closure in line:
-               new_profile.append(line) 
+    if not closure in line:
+        new_profile.append(line) 
+
 
 #Now create rules from awk logs
-with open(awk_logs,'r') as infile:
-            data = infile.readlines()
+
+#Capability rules
+with open(awk_caps,'r') as infile:
+    data = infile.readlines()
+
+for line in data:
+    new_profile.append('capability ' + line + ',')
+
+#Network rules
+with open(awk_net,'r') as infile:
+    data = infile.readlines()
+
+for line in data:
+    new_profile.append('network ' + line + ',')
+
+#File rules
+with open(awk_caps,'r') as infile:
+    data = infile.readlines()
+
+for line in data:
+    line = line.split(' ')
+    permission = line[1]
+    if line[1] == 'c': #There is no create permission in apparmor so we change it to write
+        permission = 'w'
+    if line[1] == 'x' #x must follow i,p,c,u so if there is none of these with x we give i permission
+        permission = 'ix'
+    new_profile.append(line[0] + ' ' + permission + ',')
+
+#End of logs so close the bracket
+new_profile.append('}\n')
 
 
-capability = #Capability
-network = #Network
-file_ = #File
-
-            cap = 'capability' + data[i]
-            new_profile.append(cap)
-        if network in data[i]:
-            net = 'network' + data[i]
-        if file_ in data[i]:
-            file_r = 
 #Delete duplicate rules by converting list to set. Convert back to list to keep the order of the beggining and ending of a profile
 #This is the way to delete duplicates, when we don't care about the order
 new_profile = list(set(new_profile))
 
-#Delete duplicates, but keeping the order JUST FOR THE PRESENTATION in order to show the extracted rules from the correct orders
-#static_profile = ordered_set(static_profile)
-
-#Add the beggining and ending of the profile - do it in both inordered and not inoerdered lists
-#beggining
+#Add the base of the profile in the beginning
+#ADD BASE HERE LINE BY LINE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 static_profile.insert(0, '#include <tunables/global>\n\nprofile new_profile flags=(attach_disconnected,mediate_deleted) {\n\n')
-#ending
-static_profile.append('}\n')
+
+#End of logs so close the bracket
+new_profile.append('}\n')
 
 
 #Output
