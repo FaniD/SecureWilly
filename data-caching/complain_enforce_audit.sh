929b388ca561ebfdada0d398adfb14f2809dde0f
Ubuntu <ubuntu@docker-security7.novalocal>
2018-06-17 23:15:44 +0000

    new script to decide when to stop complain mode to get into enforce mode and other small changes

---
+++ data-caching/complain_enforce_audit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+read service
+read round
+
+wc -l profiles/${service}/version_round | awk '{print $1}' > f1
+echo "$((round - 1))" > round_previous 
+
+#Save all logs in files
+#mkdir Logs/RUN${round}
+#sudo cat /var/log/kern.log > Logs/RUN${round}/kernlogs_all
+#chmod 777 Logs/RUN${round}/kernlogs_all
+#sudo dmesg > Logs/RUN${round}/dmesg_all
+
+#Then separate them for each service/profile
+#for SERVICE in server client; do
+#	cat Logs/RUN${round}/kernlogs_all | grep "${SERVICE}" > Logs/RUN${round}/kernlogs_${SERVICE}
+#	cat Logs/RUN${round}/dmesg_all | grep "${SERVICE}" > Logs/RUN${round}/dmesg_${SERVICE}
+done
+
