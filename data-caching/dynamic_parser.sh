93103801ba6048126472d89a1e72104d0c4fa5dc
Ubuntu <ubuntu@docker-security8.novalocal>
2018-07-22 16:15:33 +0000

    Dynamic parser works for complain and enforce. Now working on audit part

--- data-caching/dynamic_parser.sh
+++ data-caching/dynamic_parser.sh
@@ -11,42 +11,71 @@
 
 #For each RUN follow the steps
 #Starting with complain mode
-i=1
+#i=1
+#while true; do
+#	./1_clear_containers.sh
+#	echo $i | source 2_cp_to_apparmor.sh
+#	./3_load_profiles.sh 
+#	./4a_complain_mode.sh
+#	./5_clear_logs.sh 
+#	./6_net.sh
+#	./7_run.sh
+#	./8_closing.sh
+#	echo $i | source 9_logging_files.sh
+#	echo $i | source 10a_awk_it_complain.sh
+#	x=${x:-$i}
+#	((x++))
+#	for SERVICE in server client; do  #FIX THIS -> GENERIC
+#		python 11_merge_profiles.py $SERVICE $i 'complain'
+#	done
+#	echo $x | source 12_complain_enforce_audit.sh
+#	enforce_time='1'
+#	for SERVICE in server client; do
+#		next_step=$(head -n 1 next_step_${SERVICE})
+#		echo "Next step for ${SERVICE} is $next_step"
+#		if [ $next_step == '0' ]
+#		then
+#			enforce_time='0'
+#		fi
+#	done
+#	((i++))
+#	if [ $enforce_time == '1' ] #Then none of the services has 0 value so enforce time
+#	then
+##		echo "Inside enforce time = ${enforce_time}"
+#		break
+#	fi	
+#done
+i=5
 while true; do
 	./1_clear_containers.sh
 	echo $i | source 2_cp_to_apparmor.sh
-	./3_load_profiles.sh 
-	./4a_complain_mode.sh
-	./5_clear_logs.sh 
+	./3_load_profiles.sh
+	./4b_enforce_mode.sh
+	./5_clear_logs.sh
 	./6_net.sh
 	./7_run.sh
-	./9_closing.sh
-	echo $i | source 8_logging_files.sh
-	echo $i | source awk_it_complain.sh
-#	enforce_time=1
+	./8_closing.sh
+	echo $i | source 9_logging_files.sh
+	echo $i | source 10b_awk_it_enforce.sh
 	x=${x:-$i}
 	((x++))
 	for SERVICE in server client; do  #FIX THIS -> GENERIC
-		python merge_profiles.py $SERVICE $i 'complain'
+		python 11_merge_profiles.py $SERVICE $i 'enforce'
 	done
-	#echo $SERVICE $x | source complain_enforce_audit.sh
-	echo $x | source complain_enforce_audit.sh
-	enforce_time='1'
+	echo $x | source 12_complain_enforce_audit.sh
+	audit_time='1'
 	for SERVICE in server client; do
 		next_step=$(head -n 1 next_step_${SERVICE})
 		echo "Next step for ${SERVICE} is $next_step"
 		if [ $next_step == '0' ]
 		then
-			enforce_time='0'
-#			echo "enforce time = ${enforce_time}"
+			audit_time='0'
 		fi
 	done
 	((i++))
-#	echo "enforce time = ${enforce_time}"
-	if [ $enforce_time == '1' ] #Then none of the services has 0 value so enforce time
+	if [ $audit_time == '1' ] #Then none of the services has 0 value so audit time
 	then
-#		echo "Inside enforce time = ${enforce_time}"
-#		echo "Enters here?"
 		break
-	fi	
+	fi
 done
+
