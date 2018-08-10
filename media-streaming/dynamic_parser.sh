9cf306889036719579a91f04cf0028c1a91126e1
Ubuntu <ubuntu@docker-security8.novalocal>
2018-08-10 18:36:21 +0000

    Fixed dynamic parser to omiit /var/lib/docker directory

--- media-streaming/dynamic_parser.sh
+++ media-streaming/dynamic_parser.sh
@@ -84,7 +84,7 @@ done
 #Audit flag runs (complain & enforce) I need globbing.py otherwise we'll have an infinite loop because of every new instance
 
 y=${y:-$i}
-#while true; do
+while true; do
 
 	./1_clear_containers.sh
 	if [ $y == $i ] 
@@ -107,24 +107,23 @@ y=${y:-$i}
 	for SERVICE in dataset server client; do  #FIX THIS -> GENERIC
 		python 11_merge_profiles.py $SERVICE $i 'complain'
 	done
-#	echo $x | source 12_complain_enforce_audit.sh
-#	audit_enforce_time='1'
-#	for SERVICE in dataset server client; do
-#		next_step=$(head -n 1 next_step_${SERVICE})
-#		if [ $next_step == '0' ]
-#		then
-#			audit_enforce_time='0'
-#		fi
-#	done
+	echo $x | source 12_complain_enforce_audit.sh
+	audit_enforce_time='1'
+	for SERVICE in dataset server client; do
+		next_step=$(head -n 1 next_step_${SERVICE})
+		if [ $next_step == '0' ]
+		then
+			audit_enforce_time='0'
+		fi
+	done
 	((i++))
-#	if [ $audit_enforce_time == '1' ] #Then none of the services has 0 value so audit enforce time
-#	then
-#		break
-#	fi
-#done
+	if [ $audit_enforce_time == '1' ] #Then none of the services has 0 value so audit enforce time
+	then
+		break
+	fi
+done
 
-
-#while true; do
+while true; do
 	./1_clear_containers.sh
 	echo $i | source 2_cp_to_apparmor.sh
         ./3_load_profiles.sh
@@ -140,23 +139,22 @@ y=${y:-$i}
 	for SERVICE in dataset server client; do  #FIX THIS -> GENERIC
 		python 11_merge_profiles.py $SERVICE $i 'enforce'
         done
-#	echo $x | source 12_complain_enforce_audit.sh
-#	end_of_logs='1'
-#	for SERVICE in dataset server client; do
-#		next_step=$(head -n 1 next_step_${SERVICE})
-#	        #echo "Next step for ${SERVICE} is $next_step"
-#	        if [ $next_step == '0' ]
-#	        then
-#		        end_of_logs='0'
-#		fi
-#	done
+	echo $x | source 12_complain_enforce_audit.sh
+	end_of_logs='1'
+	for SERVICE in dataset server client; do
+		next_step=$(head -n 1 next_step_${SERVICE})
+	        #echo "Next step for ${SERVICE} is $next_step"
+	        if [ $next_step == '0' ]
+	        then
+		        end_of_logs='0'
+		fi
+	done
 	((i++))
-#	if [ $end_of_logs == '1' ] #Then none of the services has 0 value so audit time
-#	then
-#	        break
-#	fi
-#done
-
+	if [ $end_of_logs == '1' ] #Then none of the services has 0 value so audit time
+	then
+	        break
+	fi
+done
 #version_{i} is the last profile
 #Delete audit flag now
 for SERVICE in dataset server client; do  #FIX THIS -> GENERIC
