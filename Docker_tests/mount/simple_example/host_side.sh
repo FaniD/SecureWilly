c4e8788531d9b9d8effa98c9acab10b76b068e23
Ubuntu <ubuntu@docker-security13.novalocal>
2018-09-19 23:33:04 +0000

    Added some scripts with the necessary steps to achieve mount to a running container

---
+++ Docker_tests/mount/simple_example/host_side.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#Layers in container's filesystem
+sudo ls /var/lib/docker/aufs/diff
+#Choose one and create a dir there
+read path
+sudo mkdir /var/lib/docker/aufs/diff/${path}/doot
+sudo touch /var/lib/docker/aufs/diff/${path}/doot/HelloFromTheOtherSide
+
+#Then mount a host directory to doot
+sudo mount -o bind /home/ubuntu/Security-on-Docker/Docker_tests/mount/simple_example/restricted_area /var/lib/docker/aufs/diff/${path}/doot
+
+#Get some info around mount
+df /home/ubuntu/Security-on-Docker/Docker_tests/mount/simple_example/restricted_area > df
+sudo cat /proc/self/mountinfo > mountinfo
+
+
+
---
+++ Docker_tests/mount/simple_example/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#Build and run docker image
+docker build . -t mo
+
+#Version 1 Worked
+docker run --privileged --security-opt "apparmor=mount_profile" -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/simple_example/data:/mount_here -t -i mo:latest
+
+#Version 2
+#docker run --privileged -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/simple_example/data:/mount_here -t -i mo:latest
