db30db2979bb226e4a8be7f578229078ead81da6
Ubuntu <ubuntu@docker-security13.novalocal>
2018-10-09 19:47:06 +0000

    Example for read-only volumes

---
+++ Docker_tests/mount/ro_volumes/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker build . -t ro_vol
+
+#In this example volume is not read-only so you can write with container to this fs
+#docker run -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/ro_volumes/data:/data -t -i ro_vol:latest
+
+#In this example volume is !!read-only!! so you cannot write with container to this fs
+docker run -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/ro_volumes/data:/data:ro -t -i ro_vol:latest
