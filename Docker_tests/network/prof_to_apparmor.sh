831df02fae310ff65fd43ab7db56046b4238f246
Ubuntu <ubuntu@docker-security13.novalocal>
2018-10-15 22:38:33 +0000

    Network example run by static and dynamic parser. Genprof tested it too.

--- Docker_tests/network/prof_to_apparmor.sh
+++ Docker_tests/network/prof_to_apparmor.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-sudo cp net_profile /etc/apparmor.d/
+
+sudo cp profiles/client/output_client_profile /etc/apparmor.d/net_profile
+#After copying this file vim the /etc/apparmor.d/net_profile and change client_profile to net_profile
 sudo apparmor_parser -r -W /etc/apparmor.d/net_profile
---
+++ Docker_tests/network/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#Run on host
+#python3 -m http.server --bind localhost
+
+#Run on host to test is server is listening
+#curl localhost:8000
+
+#Run the container to see if host we can reach host through the container
+docker build -t net_test .
+#Without profile everything works fine
+#docker run -it --rm --net=host net_test
+
+#Profile time:
+#docker run -it --rm --net=host --security-opt "apparmor=net_profile" net_test
+
+#Let's see what happens if we give net_admin capability:
+docker run -it --rm --net=host --security-opt "apparmor=net_profile" net_test
