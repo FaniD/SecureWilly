a539bc31b2865da99f7b53380e051072eff21c5c
Ubuntu <ubuntu@docker-security13.novalocal>
2018-10-27 00:00:19 +0000

    Testing ro volumes

--- Docker_tests/mount/ro_volumes/run.sh
+++ Docker_tests/mount/ro_volumes/run.sh
@@ -3,7 +3,9 @@
 docker build . -t ro_vol
 
 #In this example volume is not read-only so you can write with container to this fs
-#docker run -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/ro_volumes/data:/data -t -i ro_vol:latest
+#docker run -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/ro_volumes/data:/mount_here -t -i ro_vol:latest
 
 #In this example volume is !!read-only!! so you cannot write with container to this fs
-docker run -e HM=hmmm -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/ro_volumes/data:/data:ro -t -i ro_vol:latest
+docker run --security-opt "apparmor=ro_vol" -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/ro_volumes/data:/mount_here:ro -t -i ro_vol:latest
+
+#docker run -e HM=hmmm -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/ro_volumes/data:/mount_here:ro -t -i ro_vol:latest
--- Docker_tests/mount/simple_mount/run.sh
+++ Docker_tests/mount/simple_mount/run.sh
@@ -5,4 +5,4 @@
 
 #If you want to mount it manually and test it use docker run
 docker build -t simply .
-docker run -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/simple_mount/data:/mount_here: -t -i simply:latest
+docker run --security-opt "apparmor=mount_profile" -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/simple_mount/data:/mount_here: -t -i simply:latest
