7e24f31cc8cf130f2051b9b830fd95b59bdef9c4
Ubuntu <ubuntu@docker-security13.novalocal>
2018-10-31 00:34:26 +0000

    Changes in mount examples

--- Docker_tests/pivot_root/run.sh
+++ Docker_tests/pivot_root/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build . -t ro_vol
+docker build . -t pivot_root
 
 #In this example volume is not read-only so you can write with container to this fs
 #docker run -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/ro_volumes/data:/mount_here -t -i ro_vol:latest
@@ -8,7 +8,7 @@ docker build . -t ro_vol
 #In this example volume is !!read-only!! so you cannot write with container to this fs
 #docker run --security-opt "apparmor=ro_vol" -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/ro_volumes/data:/mount_here:ro -t -i ro_vol:latest
 
-docker run -e HM=hmmm -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/ro_volumes/data:/mount_here:ro -t -i ro_vol:latest
+#docker run -e HM=hmmm -v /home/ubuntu/Security-on-Docker/Docker_tests/mount/ro_volumes/data:/mount_here:ro -t -i ro_vol:latest
 
 
-#docker run --security-opt "apparmor=ro_vol" -t -i ro_vol:latest
+docker run --privileged -t -i pivot_root:latest
