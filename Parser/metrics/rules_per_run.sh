77bc2d1f5b8e37ed3c2b2051ff9f52d15b0caa10
Fani Dimou <fani.dimou92@gmail.com>
2018-11-27 04:21:31 +0200

    Creating a metrics shell script

--- Parser/metrics/rules_per_run.sh
+++ Parser/metrics/rules_per_run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-profile_path="../profiles/client"
+profile_path="../parser_output/profiles/client"
 
 ls ${profile_path} -1 | wc -l > num_of_runs
 
 previous=0
 for (( run=1; run<num_of_runs; run++ )) do
 	wc -l ${profile_path}/version_${run} > ver
-	version_${run} = $ver - 4 - $previous
-	previous = ${version}_${run}
+	v = $ver - 4 - $previous
+	previous = $v
 	cat previous >> increasing_rules
 done
