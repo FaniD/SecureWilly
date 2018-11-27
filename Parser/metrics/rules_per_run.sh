5d529aa24ff850fccf59d845809f29eda55724da
Fani Dimou <fani.dimou92@gmail.com>
2018-11-27 04:26:08 +0200

    Fixing metric script

--- Parser/metrics/rules_per_run.sh
+++ Parser/metrics/rules_per_run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-profile_path="../parser_output/profiles/client"
+profile_path = "client"
+#profile_path="../parser_output/profiles/client"
 
 ls ${profile_path} -1 | wc -l > num_of_runs
 
 previous=0
 for (( run=1; run<num_of_runs; run++ )) do
 	wc -l ${profile_path}/version_${run} > ver
-	v = $ver - 4 - $previous
-	previous = $v
+	echo "$((${ver} - 4 - ${previous}))" > previous
+#	v = $ver - 4 - $previous
+#	previous = $v
 	cat previous >> increasing_rules
 done
