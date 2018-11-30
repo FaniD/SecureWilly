8ee2f76238adaf4ee423069483bd3ac8c72f071c
Ubuntu <ubuntu@docker-security15.novalocal>
2018-11-30 23:20:38 +0000

    Rules_per_run.sh and plot.py are ready. Metric counting rules per run is ready

--- Parser/metrics/plot.py
+++ Parser/metrics/plot.py
@@ -14,20 +14,13 @@ rules=[] #This will be a 2d list, with services as rows and columns as totals of
 rules_services=[]
 i=1
 for SERVICE in service_list:
-    rules_services.append(str(sys.argv[i]))
+    rules_services.append(str(sys.argv[i])) #reading paths of files with totals per run
     rules.append([]) #initiate list of each row
     i+=1
 
-#rules_dataset = str(sys.argv[1])
-#rules_client = str(sys.argv[2])
-#rules_server = str(sys.argv[3])
-
-#Fix this argv as needed
-num_of_runs = str(sys.argv[4])
-
 #Create int lists from string arrays
+#Reading per line and appending list
 i=0
-#rules=[]
 for SERVICE in service_list:
     with open(rules_services[i],'r') as infile:
         data = infile.readlines()
@@ -50,15 +43,10 @@ for x in range(int(num_of_runs)):
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(x_Axis, rules[0], label="dataset", linewidth=0.3, color="green", marker='x')
+ax.plot(x_Axis, rules[0], label="dataset", color="green", marker='x')
 ax.plot(x_Axis, rules[1], label="client", color="red", marker='x')
 ax.plot(x_Axis, rules[2], label="server", color="blue", marker='x')
-"""
-leg_lines = plt.get_lines()
-leg_texts = plt.get_texts()
-# bulk-set the properties of all lines and texts
-plt.setp(leg_lines, linewidth=4)
-"""
+
 ax.legend(loc=0)
 ax.grid()
 ax.set_xlabel("Runs")
@@ -66,4 +54,4 @@ ax.set_ylabel(r"Rules")
 ax.set_ylim(0,max_value+2)
 plt.show()
 plt.title("Rules per run")
-plt.savefig("rules.png",bbox_inches="tight")
+plt.savefig("../../parser_output/rules.png",bbox_inches="tight")
