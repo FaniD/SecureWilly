447a72dc87d8773a18ac2d5083c0dd935fc2a81f
Ubuntu <ubuntu@docker-security17.novalocal>
2018-12-04 01:52:02 +0000

    Bash script small changes

--- media-streaming_latest/Parser/dynamic_parser.sh
+++ media-streaming_latest/Parser/dynamic_parser.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 #Change this with the services I have each time
-#Also do that in 2_cp, 3, 4a, 4b, 9, 10a, 10b, 12
+#Also do that in 2_cp, 3, 4a, 4b, 9, 10a, 10b, 12, metrics 
 service_list=(dataset server client)
 
 app_run_path=".."
 parser_path="${app_run_path}/Parser"
-dynamic_script_path="${parser_path}/dynamicscripts"
+dynamic_script_path="${parser_path}/dynamic_scripts"
 
 #Clear old files and output directories and create new
 mkdir ${app_run_path}/parser_output
 
 #If static analysis has been done, then we expect to see static profile in output directory. Count files in there to find out
-ls ${app_run_path}/parser_output/ -1 | wc -l > static_part
+ls ${app_run_path}/parser_output/ -1 | wc -l > s
+static_part=$(head -n 1 s)
 
 rm -r ${app_run_path}/parser_output/Logs
 mkdir ${app_run_path}/parser_output/Logs
@@ -21,14 +22,14 @@ rm -r ${app_run_path}/parser_output/profiles
 mkdir ${app_run_path}/parser_output/profiles
 
 for SERVICE in "${service_list[@]}"; do
-#	sudo rm /etc/apparmor.d/${SERVICE}_profile
+	sudo rm /etc/apparmor.d/${SERVICE}_profile
 	mkdir ${app_run_path}/parser_output/profiles/${SERVICE}
 	#If static profile exists, otherwise make this a comment and create it a simple version_1
-#	if [ static_part > 0 ] then
+	if [[ $static_part > 0 ]]; then
 		cp ${app_run_path}/parser_output/${SERVICE}_static_profile ${app_run_path}/parser_output/profiles/${SERVICE}/version_1
-#	else
-#		./${dynamic_script_path}/create_version_1.sh
-#	fi
+	else
+		python ${dynamic_script_path}/create_version_1.py ${SERVICE}
+	fi
 done
 
 #First of all a task that has to be done is aborting network rule if we're about to use dynamic analysis.
@@ -38,12 +39,12 @@ done
 #It cannot be aborted by its own because there will be no duplicate rule. So we abort it manually, if it is already in our profile.
 
 for SERVICE in "${service_list[@]}"; do
-	python ${dynamic_script_path}/abort_network_rule.py $SERVICE
+	python ${dynamic_script_path}/0a_abort_network_rule.py $SERVICE
 done
 
 #Pull images if there are on dockerhub and not locally
 #~~~Needs manual changes
-./${dynamic_script_path}/0_pull_images.sh
+./${dynamic_script_path}/0b_pull_images.sh
 
 
 #For each RUN follow the steps
@@ -101,7 +102,7 @@ while true; do
 	echo $i | source ${dynamic_script_path}/10b_awk_it_enforce.sh
 	x=${x:-$i}
 	((x++))
-	for SERVICE in "${service_list[@]}"; do  #FIX THIS -> GENERIC
+	for SERVICE in "${service_list[@]}"; do 
 		python ${dynamic_script_path}/11_merge_profiles.py $SERVICE $i 'enforce'
 	done
 	echo $x | source ${dynamic_script_path}/12_complain_enforce_audit.sh
@@ -144,7 +145,7 @@ y=${y:-$i}
 	echo $i | source ${dynamic_script_path}/10a_awk_it_complain.sh
 	x=${x:-$i}
 	((x++))
-	for SERVICE in "${service_list[@]}"; do  #FIX THIS -> GENERIC
+	for SERVICE in "${service_list[@]}"; do 
 		python ${dynamic_script_path}/11_merge_profiles.py $SERVICE $i 'complain'
 	done
 #	echo $x | source 12_complain_enforce_audit.sh
@@ -177,7 +178,7 @@ y=${y:-$i}
 	echo $i | source ${dynamic_script_path}/10b_awk_it_enforce.sh
 	x=${x:-$i}
         ((x++))
-	for SERVICE in "${service_list[@]}"; do  #FIX THIS -> GENERIC
+	for SERVICE in "${service_list[@]}"; do 
 		python ${dynamic_script_path}/11_merge_profiles.py $SERVICE $i 'enforce'
         done
 #	echo $x | source 12_complain_enforce_audit.sh
@@ -199,6 +200,7 @@ y=${y:-$i}
 
 #version_{i} is the last profile
 #Delete audit flag now
-for SERVICE in "${service_list[@]}"; do  #FIX THIS -> GENERIC
+for SERVICE in "${service_list[@]}"; do
 	python ${dynamic_script_path}/13_delete_audit_flag.py $SERVICE $i
+	rm next_step_${SERVICE}
 done
