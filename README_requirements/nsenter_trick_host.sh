9ad90eae7f6a6dd748a0a39c830952e047f7f38b
Ubuntu <ubuntu@docker-security15.novalocal>
2018-12-07 02:27:21 +0000

    Added nsenter install (with a trick) to requirements.sh

--- README_requirements/nsenter_trick_host.sh
+++ README_requirements/nsenter_trick_host.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## from different shell - on the host" > nsenter_trick_host.sh
-docker cp nsenter:/util-linux/nsenter /usr/local/bin/
-docker cp nsenter:/util-linux/bash-completion/nsenter /etc/bash_completion.d/nsenter
+sudo docker cp nsenter:/util-linux/nsenter /usr/local/bin/
+sudo docker cp nsenter:/util-linux/bash-completion/nsenter /etc/bash_completion.d/nsenter
 
 
