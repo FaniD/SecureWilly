52bed5202f9173962abd563b2cfd148377c7b7d7
Ubuntu <ubuntu@docker-security19.novalocal>
2018-12-18 03:31:53 +0000

    FIguring out what docker inspect attack needs

--- Attacks/docker-inspect/inspect_container/set_profile.sh
+++ Attacks/docker-inspect/inspect_container/set_profile.sh
@@ -3,3 +3,6 @@
 #Write profile to apparmor
 sudo cp inspect_attack /etc/apparmor.d
 sudo apparmor_parser -r -W /etc/apparmor.d/inspect_attack
+
+sudo cp inspect_attacker /etc/apparmor.d
+sudo apparmor_parser -r -W /etc/apparmor.d/inspect_attacker
