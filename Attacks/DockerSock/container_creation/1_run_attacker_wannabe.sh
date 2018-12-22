3d8f9438e388825fd2dd420c740cbc33fcfbc0c4
Ubuntu <ubuntu@docker-security19.novalocal>
2018-12-22 00:24:16 +0000

    2 attack examples for docker.sock and how to prevent from it

--- Attacks/DockerSock/container_creation/1_run_attacker_wannabe.sh
+++ Attacks/DockerSock/container_creation/1_run_attacker_wannabe.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker build . -t docker_socket_attack
-docker run --name attacker --rm -it --security-opt "apparmor=socket_attacker" -v /var/run/docker.sock:/var/run/docker.sock docker_socket_attack
+docker run --name attacker --rm -it --security-opt "apparmor=socket_attacker" -v /var/run/docker.sock:/var/run/docker.sock:ro docker_socket_attack
 
 #docker run --name attacker --rm -it -v /var/run/docker.sock:/var/run/docker.sock docker_socket_attack
--- Attacks/DockerSock/container_creation/set_profile.sh
+++ Attacks/DockerSock/container_creation/set_profile.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 #Write profile to apparmor
-sudo cp socket_attacked /etc/apparmor.d
-sudo apparmor_parser -r -W /etc/apparmor.d/socket_attacked
-
 sudo cp socket_attacker /etc/apparmor.d
 sudo apparmor_parser -r -W /etc/apparmor.d/socket_attacker
