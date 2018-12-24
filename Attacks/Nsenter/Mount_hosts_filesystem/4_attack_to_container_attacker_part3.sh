3deefb5e6a23410a880446e383173741cf791901
Ubuntu <ubuntu@docker-security20.novalocal>
2018-12-24 18:53:58 +0000

    Splitting up tasks in Mount host's filesystem example

---
+++ Attacks/Nsenter/Mount_hosts_filesystem/4_attack_to_container_attacker_part3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+attack="/home/ubuntu/Security-on-Docker/Attacks/Nsenter/Mount_hosts_filesystem"
+
+container_pid=$(cat PID)
+rm PID
+
+nsenter --target ${container_pid} --mount --pid -- umount /tmpmount
+
+rm dockerps
+rm containerid
+rm major_num
+rm minor_num
+rm sdev_of_fs
