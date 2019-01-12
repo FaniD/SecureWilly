3a54b2d9943cb9c60aaddd32c5e2130d7516a329
Ubuntu <ubuntu@docker-security22.novalocal>
2019-01-12 01:42:38 +0000

    Strace works (at least in complain mode). Used strace on nsenter but no chroot found.

--- Attacks/Nsenter/Breakout_to_Host/strace_nsenter.sh
+++ Attacks/Nsenter/Breakout_to_Host/strace_nsenter.sh
@@ -3,8 +3,9 @@
 #Write profile to apparmor
 sudo cp attacker_profile /etc/apparmor.d
 sudo apparmor_parser -r -W /etc/apparmor.d/attacker_profile
+sudo aa-complain /etc/apparmor.d/attacker_profile
 
-docker run --rm -it --security-opt "apparmor=attacker_profile" --pid=host --cap-add SYS_PTRACE --cap-add SYS_ADMIN benhall/strace-ubuntu
 #Inside the container run strace nsenter -t 1 -m ls /
 #Returns nothing
+docker run --rm -it --security-opt "apparmor=attacker_profile" --pid=host --cap-add SYS_PTRACE --cap-add SYS_ADMIN myubuntu fani/strace-ubuntu
 
