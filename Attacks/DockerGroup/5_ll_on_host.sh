8ae0f8f66bea163998c31c1707e36bb5c5047968
Ubuntu <ubuntu@docker-security24.novalocal>
2019-01-19 15:16:51 +0000

    Small changes at attack DockerGroup

---
+++ Attacks/DockerGroup/5_ll_on_host.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ls -l /etc/ | grep Hello #Check file's ownership
--- Attacks/DockerGroup/set_profile.sh
+++ Attacks/DockerGroup/set_profile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #Write profile to apparmor
-sudo cp attacker_profile /etc/apparmor.d
+sudo cp attacker_profile /etc/apparmor.d/
 sudo apparmor_parser -r -W /etc/apparmor.d/attacker_profile
