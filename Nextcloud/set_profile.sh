7fa0017305a26d3e11a51f503fd2f0d870b09307
Fani Dimou <fani.dimou92@gmail.com>
2019-02-09 04:27:03 +0200

    Nextcloud minimal example ready to be tested with SecureWilly

--- Nextcloud/set_profile.sh
+++ Nextcloud/set_profile.sh
@@ -3,4 +3,3 @@
 #Write profile to apparmor
 sudo cp nextcloud_profile /etc/apparmor.d
 sudo apparmor_parser -r -W /etc/apparmor.d/nextcloud_profile
-sudo aa-complain /etc/apparmor.d/nextcloud_profile
--- Nextcloud/testplan.sh
+++ Nextcloud/testplan.sh
@@ -4,7 +4,7 @@
 docker exec -u www-data -ti nextcloud_securewilly php occ maintenance:install --database "mysql" --database-name "nextcloud" --database-host "db" --database-user "willy" --database-pass "secret" --admin-user "willy" --admin-pass "secret"
 
 #Create a file in local data directory
-sudo echo "hello" > data/willy/files/HelloFromTheOtherSide
+sudo touch data/willy/files/HelloFromTheOtherSide
 
 #Use occ files:scan to make it visible to the web interface
 docker exec -u www-data -ti nextcloud_securewilly php occ files:scan --all
