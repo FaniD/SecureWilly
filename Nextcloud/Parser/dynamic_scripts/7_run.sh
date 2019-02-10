14eebe7c8ad1091a85b232f893dc21b8cdd09967
Ubuntu <ubuntu@docker-security43.novalocal>
2019-02-10 01:55:08 +0000

    Fixing bugs in nextcloud example

--- Nextcloud/Parser/dynamic_scripts/7_run.sh
+++ Nextcloud/Parser/dynamic_scripts/7_run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker-compose up -d
-sudo touch /var/lib/docker/volumes/nextcloud_nextcloud/_data/notInitialized
-sudo rsync -r /var/lib/docker/volumes/nextcloud_nextcloud/_data container:nextcloud_securewilly
-sudo rm -f /var/lib/docker/volumes/nextcloud_nextcloud/_data/notInitialized
+#sudo touch /var/lib/docker/volumes/nextcloud_nextcloud/_data/notInitialized
+#sudo rsync -r /var/lib/docker/volumes/nextcloud_nextcloud/_data container:nextcloud_securewilly
+#sudo rm -f /var/lib/docker/volumes/nextcloud_nextcloud/_data/notInitialized
 ./testplan.sh
--- Nextcloud/testplan.sh
+++ Nextcloud/testplan.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+sudo rm -r /home/ubuntu/SecureWilly/Nextcloud/data
+mkdir /home/ubuntu/SecureWilly/Nextcloud/data
 sudo chown www-data:www-data /home/ubuntu/SecureWilly/Nextcloud/data
+
+docker exec -u www-data -ti nextcloud_securewilly php occ status > answer
+answer=$(cat answer | grep 'Nextcloud is not installed')
+while [[ "$answer" == ""]]
+do
+	rm answer
+	docker exec -u www-data -ti nextcloud_securewilly php occ status > answer
+	answer=$(cat answer | grep 'Nextcloud is not installed')
+done
 
 #Configure nextcloud
 docker exec -u www-data -ti nextcloud_securewilly php occ maintenance:install --database "mysql" --database-name "nextcloud" --database-host "db" --database-user "willy" --database-pass "secret" --admin-user "willy" --admin-pass "secret"
