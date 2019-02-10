6e827d74c9e2e54d79a011b932e8051c032c0415
Ubuntu <ubuntu@docker-security43.novalocal>
2019-02-10 01:04:56 +0000

    Trying to fix nextcloud example bugs

--- Nextcloud/Parser/dynamic_scripts/7_run.sh
+++ Nextcloud/Parser/dynamic_scripts/7_run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 docker-compose up -d
+sudo touch /var/lib/docker/volumes/nextcloud_nextcloud/_data/notInitialized
+sudo rsync -r /var/lib/docker/volumes/nextcloud_nextcloud/_data container:nextcloud_securewilly
+sudo rm -f /var/lib/docker/volumes/nextcloud_nextcloud/_data/notInitialized
 ./testplan.sh
--- Nextcloud/Parser/testplan.sh
+++ Nextcloud/Parser/testplan.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-sudo chown www-data:www-data data
+sudo chown www-data:www-data /home/ubuntu/SecureWilly/Nextcloud/data
 
 #Configure nextcloud
 docker exec -u www-data -ti nextcloud_securewilly php occ maintenance:install --database "mysql" --database-name "nextcloud" --database-host "db" --database-user "willy" --database-pass "secret" --admin-user "willy" --admin-pass "secret"
 
 #Create a file in local data directory
-sudo touch data/willy/files/HelloFromTheOtherSide
+sudo touch /home/ubuntu/SecureWilly/Nextcloud/data/willy/files/HelloFromTheOtherSide
 
 #Use occ files:scan to make it visible to the web interface
 docker exec -u www-data -ti nextcloud_securewilly php occ files:scan --all
