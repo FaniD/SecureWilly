b123f1c8aa51f1edee7d104e22201fe61386f817
Ubuntu <ubuntu@docker-security46.novalocal>
2019-02-21 21:16:34 +0000

    Ready to test in next vm... Destroying this one due to zombie containers

---
+++ next_only/Parser/dynamic_scripts/0b_pull_images.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+docker pull cloudsuite/media-streaming:dataset
+docker pull cloudsuite/media-streaming:server
+docker pull cloudsuite/media-streaming:client
---
+++ next_only/Parser/metrics/rules_per_run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+service_list=(dataset server client) 
+app_path="../.."
+parser_output_path="${app_path}/parser_output"
+
+for SERVICE in "${service_list[@]}"; do
+	profile_path="${parser_output_path}/profiles/${SERVICE}"
+	
+	#Count how many runs there have been
+	ls ${profile_path} -1 | wc -l > num_of_runs
+
+	previous=0
+	#Look inside directory and run for every version file
+	for run in ${profile_path}/*; do
+		#count lines
+		wc -l $run | awk '{print $1}' > f
+		f1=$(head -n 1 f)
+		#Decrease by 4 which are the intro and closure of the profile, that shouldn't be included in rules
+		echo "$((${f1} - 4))" > fr
+		previous=$(head -n 1 fr)
+		echo $previous >> ${parser_output_path}/rules_${SERVICE}
+	done
+	rm f
+	rm fr
+done
+num_runs=$(head -n 1 num_of_runs)
+rm num_of_runs
+
+./complain_enforce_audit.sh
+
+#Do this manually depending on services
+python plot_rules_total.py ${parser_output_path}/rules_dataset ${parser_output_path}/rules_client ${parser_output_path}/rules_server $num_runs
+
+rm changes
+rm run*
+
+for SERVICE in "${service_list[@]}"; do
+	rm ${parser_output_path}/rules_${SERVICE}
+done
