78394e84d40470c8e3b0642d4a14072fc27664ac
Fani Dimou <fani.dimou92@gmail.com>
2019-02-22 18:23:02 +0200

    Getting rid of useless files

--- next/set_profile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#Write profile to apparmor
-sudo cp nextcloud_profile /etc/apparmor.d
-sudo apparmor_parser -r -W /etc/apparmor.d/nextcloud_profile
--- testing_API/Parser/dynamic_scripts/7_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
- 
-docker run test --security-opt apparmor=test_profile -p 4:6
-docker run db --security-opt apparmor=db_profile -v a:a
-docker run db --security-opt apparmor=db_profile -p 4:4 -p 5:5
-docker run test --security-opt apparmor=test_profile --ulimit nofile=1024:1024
-docker run test --security-opt apparmor=test_profile -p 80:80 -v a:b --cap-add SYS_CHROOT --cap-drop CHOWN
