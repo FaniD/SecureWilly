c379df0b019098bca4ef563ce494bf96ffcb8478
Ubuntu <ubuntu@docker-security59.novalocal>
2019-02-27 15:19:48 +0000

    Added the option of just using container's port in ports docker compose

--- Parser/static_parser.py
+++ Parser/static_parser.py
@@ -229,11 +229,14 @@ for i in xrange(len(data)): #because we will need the next line
             ports = ports.replace('/','')
             ports = ports.strip('\n')
 	    ports = ports.strip('"')
-	    ports = ports.split(':')
-	    port_host = ports[0].strip('-')
-	    port_host = port_host.strip()
-	    port_host = port_host.strip('"')
-	    port_container = ports[1]
+            if ':' in line:
+	        ports = ports.split(':')
+	        port_host = ports[0].strip('-')
+	        port_host = port_host.strip()
+	        port_host = port_host.strip('"')
+	        port_container = ports[1]
+            else:
+                port_container = ports
                                 
             if int(port_container) < 1024: #In order for an app to bind to ports < 1024 capability net bind service is needed
                 static_profile.append('\tcapability net_bind_service,  #This capability is needed to bind a socket to Internet domain privileged ports\n')
