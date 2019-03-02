c9e5af95dea0973e06e1d0f4ff0e03187785d0f7
Ubuntu <ubuntu@docker-security59.novalocal>
2019-03-02 02:39:57 +0000

    Gather all nextcloud instances together

---
+++ Nextcloud_instances/Nextcloud_c_e/Parser/dynamic_scripts/1_abort_network_rule.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import io
+import sys
+from collections import OrderedDict
+
+#This will be the output profile
+new_profile = []
+
+service = str(sys.argv[1])
+path = '../parser_output/profiles/' + service + '/version_1'
+
+with open(path,'r') as infile:
+    data = infile.readlines()
+
+#If network rule is encountered do nothing, anything else must be included in new profile
+for line in data:
+    if not 'network' in line:
+        new_profile.append(line)
+
+#Output
+with open(path, 'w') as outfile:
+	outfile.writelines( new_profile )
