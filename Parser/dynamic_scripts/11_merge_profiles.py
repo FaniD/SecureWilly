50dd9c9a7a9ad96749f0e5bdf02ee1516d2045db
Ubuntu <ubuntu@docker-security59.novalocal>
2019-03-02 15:43:46 +0000

    Concluded to use algorithm complain-enforce without audits

--- Parser/dynamic_scripts/11_merge_profiles.py
+++ Parser/dynamic_scripts/11_merge_profiles.py
@@ -86,11 +86,11 @@ for line in data:
 
     #Temporary files should not be included in dynamic analysis, otherwise there is risk of infinite loop and they are useless anyway as they differ from run to run
     #Omit /tmp and /var/tmp where temporary files exist in Linux
-    if '/tmp' in line:
-        continue
+    #if '/tmp' in line:
+    #    continue
 
-    if 'var/tmp' in line:
-        continue
+    #if 'var/tmp' in line:
+    #    continue
 
     if '/var/lib/docker/' in line:
         continue
