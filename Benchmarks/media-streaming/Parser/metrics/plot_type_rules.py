52c1aba8dadd7947a2c07a2612da12e0137f2797
Ubuntu <ubuntu@docker-security59.novalocal>
2019-03-12 17:12:21 +0000

    Fixing bars

--- Benchmarks/media-streaming/Parser/metrics/plot_type_rules.py
+++ Benchmarks/media-streaming/Parser/metrics/plot_type_rules.py
@@ -129,19 +129,19 @@ fig, ax = plt.subplots()
 bar_width = 0.35
 
 opacity = 0.4
-
-rects1 = ax.bar(x_Axis, capabilities, bar_width, alpha=opacity, color='r', label='capabilities')
-rects2 = ax.bar(x_Axis, network, bar_width, alpha=opacity, color='g', label='network')
-rects3 = ax.bar(x_Axis, signal, bar_width, alpha=opacity, color='m', label='signal')
-rects4 = ax.bar(x_Axis, mount, bar_width, alpha=opacity, color='k', label='mount')
-rects5 = ax.bar(x_Axis, rlimit, bar_width, alpha=opacity, color='y', label='rlimit')
-rects6 = ax.bar(x_Axis, file_rules, bar_width, alpha=opacity, color='b', label='file')
+ind = np.arange(num_of_runs)
+rects1 = ax.bar(ind, capabilities, bar_width, alpha=opacity, color='r', label='capabilities')
+rects2 = ax.bar(ind + bar_width, network, bar_width, alpha=opacity, color='g', label='network')
+rects3 = ax.bar(ind + bar_width, signal, bar_width, alpha=opacity, color='m', label='signal')
+rects4 = ax.bar(ind + bar_width, mount, bar_width, alpha=opacity, color='k', label='mount')
+rects5 = ax.bar(ind + bar_width, rlimit, bar_width, alpha=opacity, color='y', label='rlimit')
+rects6 = ax.bar(ind + bar_width, file_rules, bar_width, alpha=opacity, color='b', label='file')
 
 ax.set_xlabel('Runs')
 ax.set_ylabel(r'Rules')
 ax.set_title('Types of rules per run')
-ax.set_xticks(x_Axis)
-ax.set_xticklabels(x_Axis)
+ax.set_xticks(ind + bar_width/2)
+ax.set_xticklabels(ind)
 ax.legend()
 plt.show()
 output = "../../parser_output/Bar_types_" + service + ".png"
