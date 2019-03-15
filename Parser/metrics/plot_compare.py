0eb6c7b8547128070d812fcd3ef90721fd35cd16
Ubuntu <ubuntu@docker-security59.novalocal>
2019-03-15 15:15:01 +0000

    Updated plot and script compare

--- Parser/metrics/plot_compare.py
+++ Parser/metrics/plot_compare.py
@@ -20,12 +20,15 @@ bar_width = 0.35
 rects1 = plt.bar(index, total_runs, bar_width, color = (0.3,0.1,0.4,0.6), label='Total amount of runs')
 rects2 = plt.bar(index + bar_width, threshold, bar_width, color = (0.3,0.9,0.4,0.6), label='Threshold')
 
-plt.xlabel('Project run by SecureWilly')
+plt.xlabel('Projects run by SecureWilly')
 plt.ylabel('Runs')
 plt.title('Totals runs and threshold for each project')
 plt.xticks(index + bar_width, ('Media-streaming', 'Data-caching', 'Nextcloud'))
-plt.legend()
-    
+
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), fancybox=True, shadow=True)
+
 plt.tight_layout()
 plt.show()
 plt.savefig("./compare.png",bbox_inches="tight")
