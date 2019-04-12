12b22990a6dc3b1e71dee167b002903f598d755a
FaniD <fani.dimou92@gmail.com>
2019-04-12 04:20:41 +0300

    Added alternative to docker.io and compose

--- Requirements/requirements.sh
+++ Requirements/requirements.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 sudo apt-get update
 
+sudo apt-get install docker.io
+sudo apt-get install docker-compose
+
+#Older version
 #Upgrade docker-engine
-sudo apt-get upgrade docker-engine
+#sudo apt-get upgrade docker-engine
 
 #Install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+#sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
 
 #Apparmor & apparmor utils
 sudo apt-get install apparmor
