d7806e70c9b2add9e79790cda98f3ae8c7f97f06
Fani Dimou <fani.dimou92@gmail.com>
2019-04-13 17:30:14 +0300

    Scripts for AppArmor overhead are ready

--- Nextcloud/newtest/load_profiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Load the profiles into kernel
-service_list=(db nextcloud)
-for SERVICE in "${service_list[@]}"; do
-	sudo cp ${SERVICE}_profile /etc/apparmor.d/
-	sudo apparmor_parser -r -W /etc/apparmor.d/${SERVICE}_profile
-done
