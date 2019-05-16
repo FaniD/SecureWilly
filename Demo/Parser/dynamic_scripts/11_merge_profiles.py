583fc05ce49819b7090b487f7d26505579b16c60
FaniD <fani.dimou92@gmail.com>
2019-05-16 00:10:11 +0000

    Create a demo for the presentation of SecureWilly
    This example does not actually use all of the features
    that are added to the container, but they are used as
    example.

--- Demo/Parser/dynamic_scripts/11_merge_profiles.py
+++ Demo/Parser/dynamic_scripts/11_merge_profiles.py
@@ -80,7 +80,7 @@ for line in data:
         continue
 
 #Volumes here
-   #if abc
+   #if ( line.startswith('/etc') ):
        #continue
 
 
--- Demo/Parser/dynamic_scripts/7_run.sh
+++ Demo/Parser/dynamic_scripts/7_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
 docker build .. -t demo
-docker run -d -p "8887:8887" --pid=host --privileged --net=host --security-opt "apparmor=demo_profile" --name demo demo
+docker run -d --cap-add SETUID -p "8887:8887" --pid=host --privileged --net=host -v "/etc:/etc" --security-opt "apparmor=demo_profile" --name demo demo
 ./curl_it.sh
 docker kill demo
