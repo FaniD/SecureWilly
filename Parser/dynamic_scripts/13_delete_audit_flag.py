1b9615d25cbd0edd71e6c8a92feb34cf5c53d38c
FaniD <fani.dimou92@gmail.com>
2019-11-10 23:45:24 +0200

    Fix spaces in python

--- Parser/dynamic_scripts/13_delete_audit_flag.py
+++ Parser/dynamic_scripts/13_delete_audit_flag.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python2
+
 import io
 import sys
 from collections import OrderedDict
 
-#This will be the output profile
+# This will be the output profile
 new_profile = []
 
 service = str(sys.argv[1])
@@ -13,19 +14,19 @@ new = '../parser_output/profiles/' + service + '/version_' + version
 out = '../parser_output/profiles/' + service + '/output_' + service + '_profile'
 
 with open(new,'r') as infile:
-    data = infile.readlines()
+  data = infile.readlines()
 
 profile = 'profile'
-#In my parser I user profile name not paths. If paths are used here I have to search for paths noo. Not fixed yet.
-#path_to_profile = ''
+# The parser uses profile name not paths. If paths are used here I have to search for paths noo. Not fixed yet.
+# path_to_profile = ''
 
 for line in data:
-    if line.startswith(profile):
-        new_line = 'profile ' + service + '_profile flags=(attach_disconnected,mediate_deleted) {\n'
-        new_profile.append(new_line)
-    else:
-        new_profile.append(line)
+  if line.startswith(profile):
+    new_line = 'profile ' + service + '_profile flags=(attach_disconnected,mediate_deleted) {\n'
+    new_profile.append(new_line)
+  else:
+    new_profile.append(line)
 
-#Output
+# Output
 with open(out, 'w') as outfile:
-	outfile.writelines( new_profile )
+  outfile.writelines( new_profile )
