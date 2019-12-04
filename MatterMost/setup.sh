d040b78dd13979e896d138779eb5d0afebc62d56
FaniD <fani.dimou92@gmail.com>
2019-12-04 02:28:06 +0200

    MatterMost instance v1 is ready

--- MatterMost/setup.sh
+++ MatterMost/setup.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-mkdir -pv /home/fanilicious/Projects/SecureWilly/MatterMost/volumes/app/mattermost/{data,logs,config,plugins,client-plugins}
-docker-compose up
+mkdir -pv /home/fanilicious/Projects/SecureWilly/MatterMost/volumes/app/mattermost/{data,logs,config,plugins,client/plugins}
 sudo chown -R 1000:1000 /home/fanilicious/Projects/SecureWilly/MatterMost/volumes/app/mattermost/
-sudo chown -R 1000:1000 /home/fanilicious/Projects/SecureWilly/MatterMost/volumes
 docker-compose up
-
-#sudo sed -i "595s,.*,," volumes/db/
-#sudo sed -i "595s,.*,," volumes/db/
-#sudo sed -i "595s,.*,," volumes/db/
-
-#mattermost user create --firstname Willy --system_admin --email faniliciousd@gmail.com --username willy --password secret
-#mattermost team create --name SW --display_name "SecureWilly Team"
-#mattermost team add SW faniliciousd@gmail.com willy
+sleep 20
+docker exec matterapp mattermost user create --firstname Willy --system_admin --email faniliciousd@gmail.com --username willy --password secret12345_SW
+docker exec matterapp mattermost team create --name securewilly --display_name "SecureWilly Team"
+docker exec matterapp mattermost team add securewilly faniliciousd@gmail.com willy
 
